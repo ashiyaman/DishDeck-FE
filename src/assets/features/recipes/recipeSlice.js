cd94d2512f1a1411143877f449007fbeb5d835d2
ashiyaman <ashiya1005@gmail.com>
2025-03-29 17:16:16 +0530

    fix: Add deployed URL

--- src/assets/features/recipes/recipeSlice.js
+++ src/assets/features/recipes/recipeSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const DISHDECK_URI = `http://localhost:3000`
+const DISHDECK_URI = `https://dish-deck-be.vercel.app/`
 
 // Get All recipes from db
 export const fetchRecipes = createAsyncThunk('fetch/recipes', async () => {
