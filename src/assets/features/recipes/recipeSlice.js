ed549230a7ea4ad4078527f7e59e532eed706447
ashiyaman <ashiya1005@gmail.com>
2025-03-29 17:20:59 +0530

    fix: Fix errors

--- src/assets/features/recipes/recipeSlice.js
+++ src/assets/features/recipes/recipeSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const DISHDECK_URI = `https://dish-deck-be.vercel.app/`
+const DISHDECK_URI = `https://dish-deck-be.vercel.app`
 
 // Get All recipes from db
 export const fetchRecipes = createAsyncThunk('fetch/recipes', async () => {
