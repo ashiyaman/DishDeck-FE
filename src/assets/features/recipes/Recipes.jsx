7877d54f74ddb866b8ac05e536aed5b5932b877b
ashiyaman <ashiya1005@gmail.com>
2025-05-28 14:25:17 +0530

    fix: fix errors

--- src/assets/features/recipes/Recipes.jsx
+++ src/assets/features/recipes/Recipes.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { deleteRecipeById, fetchRecipeById, fetchRecipes } from './recipeSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { isActualImage } from '../../utils/ValidationUtils'
+import { isActualImage } from '../../../utils/ValidationUtils'
 import AlertMessage from '../../components/AlertMessage'
 
 const Recipes = () => {
